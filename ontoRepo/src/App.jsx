571e70a20dfc5b1651236cdda0c160b57ca687c5
Kyl0Ren <fenitrasword1@gmail.com>
2024-03-19 12:45:53 +0300

    login final

--- ontoRepo/src/App.jsx
+++ ontoRepo/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = ()=>{
+  return(
+    <div className='back'>
+      <div className='container'>
+      <div className='left'></div>
+      <div className='right'>
+       <form action="">
+       <h1>Welcome!</h1>
+        <p className='mini'>log in your account</p>
+        <label htmlFor="">Email</label>
+        <input type="Email" placeholder='Usernme / yours@email.com' />
+        <label htmlFor="">Password</label>
+        <input type="Email" placeholder='Usernme / yours@email.com' />
+        <button type='submit'>Login</button>
+       </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
